f1714e5cd144ae7e558acdb67e31370993102a79
Jacob Neterer <jacobrneterer@gmail.com>
2020-04-10 17:29:05 -0400

    Adding Cloudinary Support for Images
    Adding cloudinary support for images that showcase an issue where image placeholders breaks. Submitting ticket as well

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,6 +2,8 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { CloudinaryModule } from '@cloudinary/angular-5.x';
+import * as Cloudinary from 'cloudinary-core';
 import { InlineSVGModule } from 'ng-inline-svg';
 import { ClientService } from '../services/client.service';
 import { SearchService } from '../services/search.service';
@@ -13,11 +15,13 @@ import { MaterialModule } from './material.module';
     CommonModule,
     FormsModule,
     InlineSVGModule.forRoot(),
+    CloudinaryModule.forRoot(Cloudinary, { cloud_name: 'angular-starters'}),
     MaterialModule,
     RouterModule,
   ],
   exports: [
     InlineSVGModule,
+    CloudinaryModule,
     FormsModule,
     MaterialModule,
     RouterModule,
