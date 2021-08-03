bc49bfd37ae9422bebeb9034a0126d3e8436374a
Jacob Neterer <jacobrneterer@gmail.com>
2021-08-03 11:02:59 -0400

    Saves the user's profile if it doesn't exist or updates from GitHub

---
+++ src/app/contracts/user/profile.ts
@@ -0,0 +1,6 @@
+export interface UserProfile {
+  id: string;
+  full_name: string;
+  avatar_url: string;
+  user_name: string;
+}
--- src/app/shared/services/supabase/supabase.service.ts
+++ src/app/shared/services/supabase/supabase.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { AuthChangeEvent, createClient, Session, SupabaseClient, User } from '@supabase/supabase-js';
+import { AuthChangeEvent, createClient, PostgrestResponse, Session, SupabaseClient, User } from '@supabase/supabase-js';
+import { UserProfile } from 'contracts/user/profile';
 import { environment } from 'environment';
 import { BehaviorSubject, from, Observable, of, throwError } from 'rxjs';
 import { mergeMap } from 'rxjs/operators';
@@ -18,7 +19,13 @@ export class SupabaseService {
     this.supabase = createClient(environment.supabaseUrl, environment.supbaseKey);
     this.supabase.auth.onAuthStateChange((event: AuthChangeEvent, session: Session | null) => {
       this.session.next(session);
-      this.user.next(session ? session.user : null);
+      if (session) {
+        const user: User | null = session.user;
+        this.user.next(session.user);
+        if (user) this.updateUserProfile(user);
+      } else {
+        this.user.next(null);
+      }
     });
   }
 
@@ -57,6 +64,29 @@ export class SupabaseService {
    */
   get supabaseSession(): Session | null {
     return this.session.getValue();
+  }
+
+  /**
+   * Given the current session's user, determines if the user's profile
+   * exists in the DB. If not, it creates it. Otherwise, it determines if
+   * the user's profile has been updated from GitHub and updates it in the DB.
+   * @param {User} user
+   */
+  updateUserProfile(user: User): void {
+    // Get the user's current profile. If it hasn't been saved before, or it has been updated from GitHub,
+    // then upsert it (save or update).
+    from(this.supabase.from('profiles').select('id, full_name, avatar_url, user_name'))
+      .subscribe(({ data }: PostgrestResponse<UserProfile>) => {
+        const userProfile: UserProfile | undefined | null = data ? data.find(({ id }: UserProfile) => id === user.id) : null;
+        if (!userProfile || !!userProfile && (userProfile.full_name !== user.user_metadata.full_name || userProfile.avatar_url !== user.user_metadata.avatar_url || userProfile.user_name !== user.user_metadata.user_name)) {
+          from(this.supabase.from('profiles').upsert({
+            ...user.user_metadata,
+            id: user.id,
+          }, {
+            returning: 'minimal',
+          })).subscribe();
+        }
+      });
   }
 
 }
