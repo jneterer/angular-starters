e10a66ffe7b701c9e4d4e30820585d8fb3e74063
Jacob Neterer <jacobrneterer@gmail.com>
2021-08-02 22:22:19 -0400

    Creating basic responsive private layout

--- src/app/private/private.component.ts
+++ src/app/private/private.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { User } from '@supabase/supabase-js';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { SupabaseService } from 'shared/services/supabase/supabase.service';
 
 @Component({
   selector: 'app-private',
   templateUrl: './private.component.html',
   styleUrls: ['./private.component.scss']
 })
-export class PrivateComponent implements OnInit {
+export class PrivateComponent implements OnInit, OnDestroy {
+  menuOpen: boolean = false;
+  user: User | null = null;
+  private unsubscribe: Subject<any> = new Subject<any>();
 
-  constructor() { }
+  constructor(private supabaseService: SupabaseService) { }
 
   ngOnInit(): void {
+    this.supabaseService.$user.pipe(
+      takeUntil(this.unsubscribe)
+    ).subscribe((user: User | null) => this.user = user);
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe.next();
+    this.unsubscribe.complete();
   }
 
 }
--- src/app/shared/services/supabase/supabase.service.ts
+++ src/app/shared/services/supabase/supabase.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { AuthChangeEvent, createClient, Session, SupabaseClient } from '@supabase/supabase-js';
+import { AuthChangeEvent, createClient, Session, SupabaseClient, User } from '@supabase/supabase-js';
 import { environment } from 'environment';
 import { BehaviorSubject, from, Observable, of, throwError } from 'rxjs';
 import { mergeMap } from 'rxjs/operators';
@@ -11,10 +11,15 @@ export class SupabaseService {
   private supabase: SupabaseClient;
   private session: BehaviorSubject<Session | null> = new BehaviorSubject<Session | null>(null);
   public $session: Observable<Session | null> = this.session.asObservable();
+  private user: BehaviorSubject<User | null> = new BehaviorSubject<User | null>(null);
+  public $user: Observable<User | null> = this.user.asObservable();
 
   constructor() {
     this.supabase = createClient(environment.supabaseUrl, environment.supbaseKey);
-    this.supabase.auth.onAuthStateChange((event: AuthChangeEvent, session: Session | null) => this.session.next(session));
+    this.supabase.auth.onAuthStateChange((event: AuthChangeEvent, session: Session | null) => {
+      this.session.next(session);
+      this.user.next(session ? session.user : null);
+    });
   }
 
   /**
