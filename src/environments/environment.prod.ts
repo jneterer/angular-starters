a472589e7e9f5bf4f8b75dc1c227c98422497b37
Jacob Neterer <jacobrneterer@gmail.com>
2021-08-06 15:52:42 -0400

    Temporarily use dev as prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  supabaseUrl: "",
-  supbaseKey: "",
-  starterCoverBucket: "",
+  supabaseUrl: "https://jxvpmuctnuawfceiblgl.supabase.co",
+  supbaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyNzkyNzc1MiwiZXhwIjoxOTQzNTAzNzUyfQ.4ysap6py79kV_Xlt7BJaU6HeAHIp0FFrWT0swB9wWXg",
+  starterCoverBucket: "starter-covers",
 };
