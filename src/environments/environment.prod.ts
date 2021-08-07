5b81be4b7e201378a1bdb05ce4fd8afaec0cd66d
Jacob Neterer <jacobrneterer@gmail.com>
2021-08-07 00:08:41 -0400

    Fix wrong redirect to link for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   supabaseUrl: "https://jxvpmuctnuawfceiblgl.supabase.co",
   supbaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyNzkyNzc1MiwiZXhwIjoxOTQzNTAzNzUyfQ.4ysap6py79kV_Xlt7BJaU6HeAHIp0FFrWT0swB9wWXg",
   starterCoverBucket: "starter-covers",
-  redirectTo: "https://angular-starters.io/sign-in",
+  redirectTo: "https://angularstarters.io/sign-in",
 };
